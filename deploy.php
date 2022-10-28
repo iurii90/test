3abef884d32a3fa6b9042d02e661c9fbb126171e
Iurii Bogatyrev <iurii6817@gmail.com>
2022-10-28 11:47:24 -0400

    deploy test 5

--- deploy.php
+++ deploy.php
@@ -5,7 +5,7 @@ ini_set('display_errors', 1);
 ini_set('display_startup_errors', 1);
 error_reporting(E_ALL);
 
-$input = "php://input";
+//$input = "php://input";
 
 	$commands = array(
 	'git fetch --all',
